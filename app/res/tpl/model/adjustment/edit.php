3a1ec32382f5a38c4d618b7174b2ccb737a0dea2
Stephan <info@sah-company.com>
2016-01-07 19:14:15 +0100

    Added tab UI for a more unique look

--- app/res/tpl/model/adjustment/edit.php
+++ app/res/tpl/model/adjustment/edit.php
@@ -44,54 +44,66 @@
         </select>
     </div>
 </fieldset>
-<fieldset class="tab">
-    <legend class="verbose"><?php echo I18n::__('adjustmentitem_legend') ?></legend>
-    <!-- grid based header -->
-    <div class="row">
-        <div class="span1">&nbsp;</div>
-        <div class="span3">
-            <label>
-                <?php echo I18n::__('adjustmentitem_label_person') ?>
-            </label>
+<div
+    class="tab-container">
+    <?php Flight::render('shared/navigation/tabs', array(
+        'tab_id' => 'adjustment-tabs',
+        'tabs' => array(
+            'adjustment-adjustmentitems' => I18n::__('adjustment_adjustmentitem_tab')
+        ),
+        'default_tab' => 'adjustment-adjustmentitems'
+    )) ?>
+    <fieldset
+        id="adjustment-adjustmentitems"
+        class="tab">
+        <legend class="verbose"><?php echo I18n::__('adjustmentitem_legend') ?></legend>
+        <!-- grid based header -->
+        <div class="row">
+            <div class="span1">&nbsp;</div>
+            <div class="span3">
+                <label>
+                    <?php echo I18n::__('adjustmentitem_label_person') ?>
+                </label>
+            </div>
+            <div class="span2">
+                <label class="number">
+                    <?php echo I18n::__('adjustmentitem_label_net') ?>
+                </label>
+            </div>
+            <div class="span2">
+                <label class="number">
+                    <?php echo I18n::__('adjustmentitem_label_vatvalue') ?>
+                </label>
+            </div>
+            <div class="span2">
+                <label class="number">
+                    <?php echo I18n::__('adjustmentitem_label_gros') ?>
+                </label>
+            </div>
+            <div class="span2">
+                <label class="number">
+                    <?php echo I18n::__('adjustmentitem_label_invoice') ?>
+                </label>
+            </div>
         </div>
-        <div class="span2">
-            <label class="number">
-                <?php echo I18n::__('adjustmentitem_label_net') ?>
-            </label>
+        <!-- end of grid based header -->
+        <!-- grid based data -->
+        <div
+            id="adjustment-<?php echo $record->getId() ?>-adjustmentitem-container"
+            class="container attachable detachable sortable">
+        <?php $_adjustmentitems = $record->with(' ORDER BY id')->ownAdjustmentitem ?>
+        <?php if (count($_adjustmentitems) == 0) $_adjustmentitems[] = R::dispense('adjustmentitem') ?>
+        <?php $index = 0 ?>
+        <?php foreach ($_adjustmentitems as $_adjustmentitem_id => $_adjustmentitem): ?>
+            <?php $index++  ?>
+            <?php Flight::render('model/adjustment/own/adjustmentitem', array(
+                'record' => $record,
+                '_adjustmentitem' => $_adjustmentitem,
+                'index' => $index
+            )) ?>
+        <?php endforeach ?>
         </div>
-        <div class="span2">
-            <label class="number">
-                <?php echo I18n::__('adjustmentitem_label_vatvalue') ?>
-            </label>
-        </div>
-        <div class="span2">
-            <label class="number">
-                <?php echo I18n::__('adjustmentitem_label_gros') ?>
-            </label>
-        </div>
-        <div class="span2">
-            <label class="number">
-                <?php echo I18n::__('adjustmentitem_label_invoice') ?>
-            </label>
-        </div>
-    </div>
-    <!-- end of grid based header -->
-    <!-- grid based data -->
-    <div
-        id="adjustment-<?php echo $record->getId() ?>-adjustmentitem-container"
-        class="container attachable detachable sortable">
-    <?php $_adjustmentitems = $record->with(' ORDER BY id')->ownAdjustmentitem ?>
-    <?php if (count($_adjustmentitems) == 0) $_adjustmentitems[] = R::dispense('adjustmentitem') ?>
-    <?php $index = 0 ?>
-    <?php foreach ($_adjustmentitems as $_adjustmentitem_id => $_adjustmentitem): ?>
-        <?php $index++  ?>
-        <?php Flight::render('model/adjustment/own/adjustmentitem', array(
-            'record' => $record,
-            '_adjustmentitem' => $_adjustmentitem,
-            'index' => $index
-        )) ?>
-    <?php endforeach ?>
-    </div>
-    <!-- end of grid based data -->
-</fieldset>
+        <!-- end of grid based data -->
+    </fieldset>
+</div>
 <!-- end of adjustment edit form -->
