f092c9519ee71a193dd910f102f21c1826dbd83a
Stephan <info@sah-company.com>
2016-01-11 03:31:10 +0100

    Added invoice cancelation to single template

--- app/res/tpl/invoice/single.php
+++ app/res/tpl/invoice/single.php
@@ -16,10 +16,17 @@
 <div class="span2 number">
     <?php echo htmlspecialchars($record->decimal('totalgros', 2)) ?>
 </div>
-<div class="span2">
+<div class="span1">
     <button 
         class="btn silent" 
         type="button"
         data-container="invoice-<?php echo $record->getId() ?>"
         data-href="<?php echo Url::build(sprintf('/invoice/payment/%d', $record->getId())) ?>"><?php echo I18n::__('invoice_label_paid_' . $record->paid) ?></button>
+</div>
+<div class="span1 number">
+    <a
+        href="<?php echo Url::build(sprintf('/invoice/cancel/%d', $record->getId())) ?>"
+        class="ir delete ask"
+        data-question="<?php echo I18n::__('invoice_confirm_cancel', null, array($record->name)) ?>"
+        title="<?php echo I18n::__('invoice_title_cancel') ?>"><?php echo I18n::__('invoice_link_cancel') ?></a>
 </div>
