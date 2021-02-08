06306b98380d98f39997576a7870eb3407c6f706
sah-comp <info@sah-company.com>
2021-02-08 11:03:12 +0100

    Added date of slaughter to mail text

--- app/res/tpl/deliverer/mail/text.php
+++ app/res/tpl/deliverer/mail/text.php
@@ -1,12 +1,13 @@
 <?php echo I18n::__('deliverer_text_mail_invoice', null, array(
     $record->invoice->name,
-    $record->invoice->localizedDate('bookingdate')
+    $record->invoice->localizedDate('bookingdate'),
+    $record->invoice->localizedDate('dateofslaughter')
 )) ?>
 --
-<?php echo htmlspecialchars( $record->invoice->company->legalname ) ?>
-<?php echo htmlspecialchars( $record->invoice->company->website ) ?>
-<?php echo htmlspecialchars( $record->invoice->company->street ) ?>
-<?php echo htmlspecialchars( $record->invoice->company->zip ) ?> <?php echo htmlspecialchars( $record->invoice->company->city ) ?>
-Telefon <?php echo htmlspecialchars( $record->invoice->company->phone ) ?>
-Fax <?php echo htmlspecialchars( $record->invoice->company->fax ) ?>
-Email <?php echo htmlspecialchars( $record->invoice->company->emailnoreply ) ?>
+<?php echo htmlspecialchars($record->invoice->company->legalname) ?>
+<?php echo htmlspecialchars($record->invoice->company->website) ?>
+<?php echo htmlspecialchars($record->invoice->company->street) ?>
+<?php echo htmlspecialchars($record->invoice->company->zip) ?> <?php echo htmlspecialchars($record->invoice->company->city) ?>
+Telefon <?php echo htmlspecialchars($record->invoice->company->phone) ?>
+Fax <?php echo htmlspecialchars($record->invoice->company->fax) ?>
+Email <?php echo htmlspecialchars($record->invoice->company->emailnoreply) ?>
