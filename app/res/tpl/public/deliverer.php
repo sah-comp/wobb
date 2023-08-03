517824eecf8203164c9f3235c408cb310a86bc04
sah-comp <info@sah-company.com>
2023-08-03 23:32:13 +0200

    Added a deliverer info page that does not need login
    It will show the current settings of all active deliverers. This page is intended to make it less needful to use a excel list.

--- app/res/tpl/public/deliverer.php
+++ app/res/tpl/public/deliverer.php
@@ -1,6 +1,23 @@
 <article class="main">
-    <section>
-        <table>
+    <section class="decay">
+        <table class="decay">
+            <caption>
+                <h1><?php echo I18n::__('app_name') ?></h1>
+                <p><?php echo count($records) . ' Lieferanten' ?></p>
+            </caption>
+            <thead class="fixable">
+                <tr>
+                    <th class="nickname">Zeichen</th>
+                    <th class="account">Konto</th>
+                    <th class="name">Lieferant</th>
+                    <th class="pricemask">Preismaske</th>
+                    <th class="maxagio">Maximaler Aufschlag</th>
+                    <th class="price">Mittwochspreis</th>
+                    <th class="reldprice">Aufschlag</th>
+                    <th class="cost">Kosten</th>
+                    <th class="bonus">Bonus</th>
+                </tr>
+            </thead>
             <tbody>
                 <?php foreach ($records as $_id => $_record) : ?>
                 <tr>
@@ -16,19 +33,19 @@
                     <td>
                         <?php echo $_record['pricingname'] ?>
                     </td>
-                    <td>
+                    <td class="numval">
                         <?php echo $_record['marginmax'] ?>
                     </td>
                     <td>
                         <?php echo $_record['pnextweekprice'] ?>
                     </td>
-                    <td>
+                    <td class="numval">
                         <?php echo $_record['reldprice'] ?>
                     </td>
-                    <td>
+                    <td class="numval">
                         <?php echo $_record['costvalue'] ?>
                     </td>
-                    <td>
+                    <td class="numval">
                         <?php echo $_record['conditionvalue'] ?>
                     </td>
                 </tr>
