17a9604b8caa67f10f0b25a5aeeb801af234f9fa
sah-comp <info@sah-company.com>
2023-10-23 16:58:43 +0200

    Close #50

--- app/res/tpl/analysis/print_overview.php
+++ app/res/tpl/analysis/print_overview.php
@@ -5,7 +5,7 @@
     <style>
         body {
             font-family: sans-serif;
-	        font-size: 9pt;
+            font-size: 9pt;
         }
         .emphasize {
             font-weight: bold;
@@ -15,11 +15,11 @@
             font-size: 13pt;
             font-weight: bold;
         }
-		.notemphasized {
-			font-weight: normal;
-			font-size: 7pt;
-			color: #666666;
-		}
+        .notemphasized {
+            font-weight: normal;
+            font-size: 7pt;
+            color: #666666;
+        }
         table {
             border-collapse: collapse;
         }
@@ -69,9 +69,9 @@
 
     <?php
     $_suppliers = $record->with(' ORDER BY id')->ownAnalysis;
-$_keys = array_keys($_suppliers);
-$_lastKey = array_pop($_keys);
-?>
+    $_keys = array_keys($_suppliers);
+    $_lastKey = array_pop($_keys);
+    ?>
 
     <!-- Totals of each supplier -->
     <div style="height: 10mm;"></div>
@@ -95,32 +95,32 @@ $_lastKey = array_pop($_keys);
             </tr>
         </thead>
         <tbody>
-        <?php foreach ($_suppliers as $_analysis_id => $_analysis): ?>
+        <?php foreach ($_suppliers as $_analysis_id => $_analysis) : ?>
             <tr>
                 <td><?php echo htmlspecialchars($_analysis->person->nickname) ?></td>
                 <td class="number"><?php echo htmlspecialchars($_analysis->decimal('piggery', 0)) ?></td>
                 <td class="number">
                     <?php
-                    if ($record->piggery != 0):
+                    if ($record->piggery != 0) :
                         echo htmlspecialchars(number_format($_analysis->piggery * 100 / $record->piggery, 2, ',', '.'));
-                    else:
+                    else :
                         ?>
                         &nsbp;
                         <?php
                     endif;
-            ?>
+                    ?>
                 </td>
                 <td class="number"><?php echo htmlspecialchars($_analysis->decimal('itwpiggery', 0)) ?></td>
                 <td class="number">
                     <?php
-                    if ($_analysis->piggery != 0 && $_analysis->itwpiggery != 0):
+                    if ($_analysis->piggery != 0 && $_analysis->itwpiggery != 0) :
                         echo htmlspecialchars(number_format($_analysis->itwpiggery / $_analysis->piggery * 100, 2, ',', '.'));
-                    else:
+                    else :
                         ?>
                         &nbsp;
                         <?php
                     endif;
-            ?>
+                    ?>
                 </td>
                 <td class="number"><?php echo htmlspecialchars($_analysis->decimal('sumweight', 3)) ?></td>
                 <td class="number"><?php echo htmlspecialchars($_analysis->decimal('sumtotalpricenetitw', 3)) ?></td>
@@ -128,6 +128,19 @@ $_lastKey = array_pop($_keys);
                 <td class="number"><?php echo htmlspecialchars($_analysis->decimal('avgweight', 3)) ?></td>
                 <td class="number"><?php echo htmlspecialchars($_analysis->decimal('avgpricenetitw', 3)) ?></td>
             </tr>
+            <?php
+            if ($_analysis->itwpiggery != 0) :
+                $fake_totalnet_without_itw = $_analysis->sumtotalpricenetitw - ($_analysis->itwpiggery * $record->company->tierwohlnetperstock);
+                $fake_avgprice_without_itw = $fake_totalnet_without_itw / $_analysis->sumweight;
+                ?>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="5" class="number notemphasized"><?php echo I18n::__('analysis_without_itw') ?></td>
+                <td class="number"><?php echo htmlspecialchars(number_format($fake_totalnet_without_itw, 3, ',', '.')) ?></td>
+                <td colspan="2">&nbsp;</td>
+                <td class="number"><?php echo htmlspecialchars(number_format($fake_avgprice_without_itw, 3, ',', '.')) ?></td>
+            </tr>
+            <?php endif ?>
         <?php endforeach ?>
             <tr>
                 <td class="bt bb emphasize"><?php echo I18n::__('analysis_label_total') ?></td>
@@ -136,14 +149,14 @@ $_lastKey = array_pop($_keys);
                 <td class="bt bb number"><?php echo htmlspecialchars($record->decimal('itwpiggery', 0)) ?></td>
                 <td class="bt bb number">
                     <?php
-                    if ($record->piggery != 0 && $record->itwpiggery != 0):
+                    if ($record->piggery != 0 && $record->itwpiggery != 0) :
                         echo htmlspecialchars(number_format($record->itwpiggery / $record->piggery * 100, 2, ',', '.'));
-                    else:
+                    else :
                         ?>
                         &nbsp;
                         <?php
                     endif;
-?>
+                    ?>
                 </td>
                 <td class="bt bb number"><?php echo htmlspecialchars($record->decimal('sumweight', 3)) ?></td>
                 <td class="bt bb number"><?php echo htmlspecialchars($record->decimal('sumtotalpricenetitw', 3)) ?></td>
