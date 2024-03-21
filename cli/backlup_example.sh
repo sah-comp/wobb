c24e2f438010c5ad25383702f4d3d971fd79164f
sah-comp <info@sah-company.com>
2024-03-21 21:30:12 +0100

    Added zipping and ftping backup to server

--- cli/backlup_example.sh
+++ cli/backlup_example.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+# to be executeable dont forget to chmod +x this file.
+
 # Crontab may look like this
 # Run backup every day a 9pm
-#0 21 * * * /Users/XYZ/Sites/wobb/cli/backup.sh >/dev/null 2>&1
+#0 21 * * * /Users/XYZ/Sites/APP/cli/backup.sh >/dev/null 2>&1
 
 # Database credentials
 user="root"
-password=""
+password="secret"
 host="localhost"
-db_name="s"
+db_name="mydatabase"
+ftpuser="ftpusername"
+ftppw="passwd"
+ftppath="host/path/to/backups"
 
 # Other options
-backup_path="/Users/XYZ//backups"
+backup_path="/Users/XYZ/path/to/Backups"
 date=$(date +"%Y-%m-%d")
 
 # Set default file permissions
 umask 177
 
 # Dump database into SQL file
-/usr/local/mysql/bin/mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
+/usr/local/bin/mysqldump --user=$user --password=$password --host=$host $db_name | gzip -c > $backup_path/$db_name-$date.sql.gz
+
+# curl the backup file to an ftp server
+curl --user $ftpuser:$ftppw --upload-file $backup_path/$db_name-$date.sql.gz ftp://$ftppath/$db_name-$date.sql.gz
 
 # Delete files older than 30 days
-#find $backup_path/* -mtime +30 -exec rm {} \;
+find $backup_path/* -mtime +30 -exec rm {} \;
