71c52c1cd6a764e317012d4f946b28a222bf4e20
Stephan Hombergs <info@sah-company.com>
2024-04-04 20:18:16 +0200

    Fixed some more parameter NULL glitches

--- app/res/tpl/scaffold/table.php
+++ app/res/tpl/scaffold/table.php
@@ -1,33 +1,33 @@
 <?php
-/**
- * Cinnebar.
- *
- * @package Cinnebar
- * @subpackage Template
- * @author $Author$
- * @version $Id$
- */
+    /**
+     * Cinnebar.
+     *
+     * @package Cinnebar
+     * @subpackage Template
+     * @author $Author$
+     * @version $Id$
+     */
 ?>
-<?php if (! $_attributes = $record->getAttributes($layout)): ?>
-    <?php if (! $_gestalt = R::findOne('gestalt', ' name = ?', array($record->getMeta('type')))): ?>
-        <?php $_attributes = array(
-            'name' => 'id',
-            'sort' => array(
-                'name' => $this->bean->getMeta('type').'.name'
-            ),
-            'filter' => array(
-                'tag' => 'number'
-            )
-        ) ?>
-    <?php else: ?>
-        <?php $_attributes = $_gestalt->getVirtualAttributes() ?>
-    <?php endif; ?>
-<?php endif ?>
-<!-- <?php echo $record->getMeta('type') ?> scaffold table -->
+<?php if ( ! $_attributes = $record->getAttributes($layout)): ?>
+<?php if ( ! $_gestalt = R::findOne('gestalt', ' name = ?', [$record->getMeta('type')])): ?>
+<?php $_attributes = [
+        'name'   => 'id',
+        'sort'   => [
+            'name' => $this->bean->getMeta('type') . '.name',
+        ],
+        'filter' => [
+            'tag' => 'number',
+        ],
+]?>
+<?php else: ?>
+<?php $_attributes = $_gestalt->getVirtualAttributes()?>
+<?php endif;?>
+<?php endif?>
+<!--<?php echo $record->getMeta('type') ?> scaffold table -->
 <table>
 
     <caption>
-        <?php echo I18n::__('scaffold_caption_index', null, array($total_records)) ?>
+        <?php echo I18n::__('scaffold_caption_index', null, [$total_records]) ?>
     </caption>
 
     <thead>
@@ -45,21 +45,21 @@
             </th>
             <!-- header attributes -->
             <?php foreach ($_attributes as $_i => $_attribute): ?>
-                <?php
-                    $_class = $record->getMeta('type').' fn-'.$_attribute['name'].' order';
-                    $_dir = 0;
-                    if ($order == $_i):
-                        $_dir = ! $dir;
-                        $_class .= ' active dir-'.$dir_map[$_dir];
-                    endif;
-                    if (isset($_attribute['class'])) {
-                        $_class .= ' '.$_attribute['class'];
-                    }
-                ?>
-            <th class="<?php echo $_class ?>" <?php echo(isset($_attribute['width']) ? 'style="width: ' . $_attribute['width'] . ';"' : '') ?>>
-                <a href="<?php echo Url::build("{$base_url}/{$type}/{$layout}/1/{$_i}/{$_dir}") ?>"><?php echo I18n::__($record->getMeta('type').'_label_'.$_attribute['name']) ?></a>
+<?php
+    $_class = $record->getMeta('type') . ' fn-' . $_attribute['name'] . ' order';
+    $_dir   = 0;
+    if ($order == $_i):
+        $_dir =  ! $dir;
+        $_class .= ' active dir-' . $dir_map[$_dir];
+    endif;
+    if (isset($_attribute['class'])) {
+        $_class .= ' ' . $_attribute['class'];
+    }
+?>
+            <th class="<?php echo $_class ?>"<?php echo (isset($_attribute['width']) ? 'style="width: ' . $_attribute['width'] . ';"' : '') ?>>
+                <a href="<?php echo Url::build("{$base_url}/{$type}/{$layout}/1/{$_i}/{$_dir}") ?>"><?php echo I18n::__($record->getMeta('type') . '_label_' . $_attribute['name']) ?></a>
             </th>
-            <?php endforeach ?>
+            <?php endforeach?>
             <!-- end of header attributes -->
         </tr>
 
@@ -99,7 +99,7 @@
             <?php foreach ($_attributes as $_i => $_attribute): ?>
             <th>
                 <?php if (isset($_attribute['filter']) && is_array($_attribute['filter'])): ?>
-                <?php $_criteria = $filter->getCriteria($_attribute) ?>
+<?php $_criteria = $filter->getCriteria($_attribute)?>
                 <input
                     type="hidden"
                     name="filter[ownCriteria][<?php echo $_i ?>][type]"
@@ -111,15 +111,15 @@
                 <input
                     type="hidden"
                     name="filter[ownCriteria][<?php echo $_i ?>][op]"
-                    value="<?php echo htmlspecialchars($_criteria->op) ?>" />
+                    value="<?php echo htmlspecialchars($_criteria->op ?? '') ?>" />
                 <input
                     type="hidden"
                     name="filter[ownCriteria][<?php echo $_i ?>][tag]"
-                    value="<?php echo htmlspecialchars($_criteria->tag) ?>" />
+                    value="<?php echo htmlspecialchars($_criteria->tag ?? '') ?>" />
                 <input
                     type="hidden"
                     name="filter[ownCriteria][<?php echo $_i ?>][attribute]"
-                    value="<?php echo htmlspecialchars($_criteria->attribute) ?>" />
+                    value="<?php echo htmlspecialchars($_criteria->attribute ?? '') ?>" />
 
                 <?php if ($_criteria->tag == 'bool'): ?>
                 <select
@@ -129,47 +129,47 @@
                         value="">
                         <?php echo I18n::__('filter_placeholder_any') ?>
                     </option>
-                    <?php foreach (array(
-                        0 => I18n::__('bool_false'),
-                        1 => I18n::__('bool_true')
-                        ) as $_bool_val => $_bool_text): ?>
+                    <?php foreach ([
+                            0 => I18n::__('bool_false'),
+                            1 => I18n::__('bool_true'),
+                    ] as $_bool_val => $_bool_text): ?>
                     <option
                         value="<?php echo $_bool_val ?>"
-                        <?php echo ($_criteria->value != '' && $_bool_val == (int)$_criteria->value) ? 'selected="selected"' : '' ?>>
+                        <?php echo ($_criteria->value != '' && $_bool_val == (int) $_criteria->value) ? 'selected="selected"' : '' ?>>
                         <?php echo $_bool_text ?>
                     </option>
-                    <?php endforeach ?>
+                    <?php endforeach?>
                 </select>
                 <?php else: ?>
                 <input
                     type="text"
-                    class="filter <?php echo(isset($_attribute['class']) ? $_attribute['class'] : '') ?>"
+                    class="filter                                  <?php echo (isset($_attribute['class']) ? $_attribute['class'] : '') ?>"
                     name="filter[ownCriteria][<?php echo $_i ?>][value]"
-                    value="<?php echo htmlspecialchars($_criteria->value) ?>"
+                    value="<?php echo htmlspecialchars($_criteria->value ?? '') ?>"
                     placeholder="<?php echo I18n::__('filter_placeholder_any') ?>" />
-                <?php endif ?>
-                <?php else: ?>
+                <?php endif?>
+<?php else: ?>
                     &nbsp;
-                <?php endif ?>
+                <?php endif?>
             </th>
-            <?php endforeach ?>
+            <?php endforeach?>
         </tr>
-        <?php endif ?>
+        <?php endif?>
 
     </thead>
 
     <tfoot>
         <tr>
-            <td colspan="<?php echo count($_attributes)+2 ?>">
+            <td colspan="<?php echo count($_attributes) + 2 ?>">
                 &nbsp;
             </td>
         </tr>
     </tfoot>
 
     <tbody>
-        <?php $offset = 0 ?>
-        <?php foreach ($records as $id => $_record): ?>
-            <?php $offset++ ?>
+        <?php $offset = 0?>
+<?php foreach ($records as $id => $_record): ?>
+<?php $offset++?>
         <tr
             id="<?php echo $_record->getMeta('type') ?>-<?php echo $_record->getId() ?>"
             class="bean bean-<?php echo $_record->getMeta('type') ?>">
@@ -177,7 +177,7 @@
             <td>
                 <a
                     class="ir action action-edit"
-                	href="<?php echo Url::build('/admin/%s/edit/%d/%d/%d/%d/%s/', array($_record->getMeta('type'), $_record->getId(), $offset, $order, $dir, $layout)) ?>">
+                	href="<?php echo Url::build('/admin/%s/edit/%d/%d/%d/%d/%s/', [$_record->getMeta('type'), $_record->getId(), $offset, $order, $dir, $layout]) ?>">
                     <?php echo I18n::__('scaffold_action_edit') ?>
                 </a>
             </td>
@@ -196,19 +196,19 @@
                 class="<?php echo (isset($_attribute['class'])) ? $_attribute['class'] : '' ?>">
                 <?php if (isset($_attribute['callback'])): ?>
 
-                    <?php echo htmlspecialchars($_record->{$_attribute['callback']['name']}($_attribute['name'])) ?>
+                    <?php echo htmlspecialchars($_record->{$_attribute['callback']['name']}($_attribute['name']) ?? '') ?>
 
                 <?php else: ?>
 
-                    <?php echo htmlspecialchars($_record->{$_attribute['name']}) ?>
+                    <?php echo htmlspecialchars($_record->{$_attribute['name']} ?? '') ?>
 
-                <?php endif ?>
+                <?php endif?>
             </td>
-            <?php endforeach ?>
+            <?php endforeach?>
             <!-- end of body attributes -->
 
         </tr>
-        <?php endforeach ?>
+        <?php endforeach?>
     </tbody>
 
 </table>
